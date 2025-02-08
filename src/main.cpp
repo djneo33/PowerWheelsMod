4396a2724ef0e39721d9a0f23715907ad402d343
djlappy <djneo33@yahoo.com>
2025-02-07 19:13:11 -0600

    v2

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include <Arduino.h>
-#define SteeringInput 4
-#define ThrottleInput 5
-#define FourInput 3
+#define SteeringInput 2
+#define ThrottleInput 3
+#define FourInput 4
 #define Ain1 1
-#define Ain2 2
+#define Ain2 5
 #define A_PWM 6
 #define Bin2 7
 #define Bin1 8
@@ -11,22 +11,25 @@
 #define L_PWM 10
 #define R_PWM 11
 
-unsigned long startTimmingThrottle;
-unsigned long startTimmingSteering;
+volatile unsigned long startTimmingThrottle;
+volatile unsigned long startTimmingSteering;
 unsigned long startTimmingFour;
 unsigned long four;
-unsigned long Steering;
-unsigned long Throttle;
+volatile unsigned long Steering;
+volatile unsigned long Throttle;
 unsigned long past;
 unsigned long highest;
 unsigned long lowest;
 bool pinhighSteering;
 bool pinhighThrottle;
 bool pinhighFour;
+unsigned long currentTime;
 
-void ReadSteering();
-void ReadThrottle();
-void ReadFour();
+
+
+void ReadPWM();
+void getthrottle();
+void getsteering();
 
 void setup()
 {
@@ -49,27 +52,35 @@ void setup()
   analogWrite(B_PWM,0);
   analogWrite(L_PWM,0);
   analogWrite(R_PWM,0);
+  attachInterrupt(digitalPinToInterrupt(3),getthrottle,CHANGE);
+  attachInterrupt(digitalPinToInterrupt(2),getsteering,CHANGE);
+
   
   delay(1000);
-}
 
+}
 void loop()
 {
 
- 
-  ReadSteering();
-  ReadThrottle();
-  ReadFour();
+ ReadPWM();
+  
+  
+
+  
  
   if (Throttle >= 1600)
   {
+   
+  if (Throttle > 2200){
+    Throttle = 1500;
+   }
     digitalWrite(Ain1, HIGH);
     digitalWrite(Ain2, LOW);
-    if (four >= 1700)
+    if (four > 1700)
     {
       digitalWrite(Bin1, HIGH);
       digitalWrite(Bin2, LOW);
-      analogWrite(B_PWM, map(Throttle, 1600, 2200, 50, 255));
+      analogWrite(B_PWM, map(Throttle, 1600, 2000, 0, 255));
     }
     else
     {
@@ -77,17 +88,23 @@ void loop()
       digitalWrite(Bin2, HIGH);
       analogWrite(B_PWM, 0);
     }
-    analogWrite(A_PWM, map(Throttle, 1600, 2200, 50, 255));
+    analogWrite(A_PWM, map(Throttle, 1600, 2000, 0, 255));
+ 
   }
+  
   if (Throttle <= 1400)
   {
+      if(Throttle < 800){
+    Throttle = 1500;
+   }
+    
     digitalWrite(Ain1, LOW);
     digitalWrite(Ain2, HIGH);
-    if (four >= 1700)
+    if (four > 1700)
     {
       digitalWrite(Bin1, LOW);
       digitalWrite(Bin2, HIGH);
-      analogWrite(B_PWM, map(Throttle, 1400, 990, 50, 255));
+      analogWrite(B_PWM, map(Throttle, 1400, 1000, 0, 255));
     }
     else
     {
@@ -95,68 +112,83 @@ void loop()
       digitalWrite(Bin2, HIGH);
       analogWrite(B_PWM, 0);
     }
-    analogWrite(A_PWM, map(Throttle, 1400, 990, 50, 255));
+   
+ 
+   
+    analogWrite(A_PWM, map(Throttle, 1400, 1000, 0, 255));
   }
+  
   if (Steering >= 1600)
+  
   {
+   if (Steering > 2200){
+    Steering = 1500;
+  }
+   
     analogWrite(R_PWM, 0);
-    analogWrite(L_PWM, map(Steering, 1600, 2200, 50, 255));
+    analogWrite(L_PWM, map(Steering, 1600, 2000, 0, 255));
   }
   if (Steering <= 1400)
+  
   {
+    if (Steering < 800){
+    Steering = 1500;
+  }
+    
     analogWrite(L_PWM, 0);
-    analogWrite(R_PWM, map(Steering, 1400, 990, 50, 255));
+    analogWrite(R_PWM, map(Steering, 1400, 1000, 0, 255));
   }
   if (Throttle >= 1400 && Throttle <= 1600){
     analogWrite(A_PWM,0);
     analogWrite(B_PWM,0);
+    digitalWrite(Ain1,0);
+    digitalWrite(Ain2,0);
+    digitalWrite(Bin1,0);
+    digitalWrite(Bin2,0);
   }
   if (Steering >= 1400 && Steering <= 1600){
     analogWrite(L_PWM,0);
     analogWrite(R_PWM,0);
   }
+ 
+}
+
+void getthrottle(){
+if(digitalRead(ThrottleInput) == HIGH){
+  startTimmingThrottle = micros();
+
+}else{
+Throttle = micros()-startTimmingThrottle;
 
 }
 
-void ReadSteering()
-{
 
-  if (digitalRead(SteeringInput) == HIGH && pinhighSteering == false)
-  {
+}
+
+void getsteering(){
+  if(digitalRead(SteeringInput) == HIGH){
     startTimmingSteering = micros();
-    pinhighSteering = true;
-  }
-  if (digitalRead(SteeringInput) == LOW && pinhighSteering == true)
-  {
-    Steering = micros() - startTimmingSteering;
-    pinhighSteering = false;
+  }else{
+    Steering = micros()-startTimmingSteering;
   }
 }
-
-void ReadThrottle()
+void ReadPWM()
 {
-  if (digitalRead(ThrottleInput) == HIGH && pinhighThrottle == false)
-  {
-    startTimmingThrottle = micros();
-    pinhighThrottle = true;
-  }
-  if (digitalRead(ThrottleInput) == LOW && pinhighThrottle == true)
-  {
-    Throttle = micros() - startTimmingThrottle;
-    pinhighThrottle = false;
-  }
-}
-
-void ReadFour()
-{
-  if (digitalRead(FourInput) == HIGH && pinhighFour == false)
-  {
-    startTimmingFour = micros();
-    pinhighFour = true;
-  }
-  if (digitalRead(FourInput) == LOW && pinhighFour == true)
-  {
-    four = micros() - startTimmingFour;
+ 
+  unsigned long currentTime = micros();
+ if (digitalRead(FourInput) == HIGH && pinhighFour == false ){
+  startTimmingFour = currentTime;
+  pinhighFour = true;
+ }
+  if (digitalRead(FourInput) == LOW && pinhighFour == true ){
+    four = currentTime - startTimmingFour;
     pinhighFour = false;
-  }
+   
+    
+
+ 
+ }
+
+
 }
+
